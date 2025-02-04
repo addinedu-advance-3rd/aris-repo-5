c301fb10a6add5ef4c3e893da6e14c74f6cf13a2
kangjjjjjww <wjddn2006@gmail.com>
2025-02-04 15:04:36 +0900

    Refactor: Code cleanup

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from module.expand_eye import expand_eye
 from module.shrink_lip import shrink_lip
 from module.contour import get_contour_image
 from module.path_planning import *
-from module.arm_path import convert_coordinates, visualize_mapped_path, arm_coordinates
+from module.arm_path import RobotPathPlanner
 
 def main(args):
 
@@ -30,37 +30,34 @@ def main(args):
     shrink_lip_image = shrink_lip(expaded_eye_image, lip_points, args.mask_padding)
 
     # 컨투어
-    contour_image = get_contour_image(shrink_lip_image, hair_mask, face_mask)
+    #contour_image = get_contour_image(shrink_lip_image, hair_mask, face_mask)
+    contour_image = get_contour_image(image, hair_mask, face_mask)
 
     # path planning
     path = path_planning(contour_image)
-    #print(path)
 
-    a,b,_ = image.shape
-    converted_path = convert_coordinates(path,a,b)
-    #print(converted_path)
-    # visualize_mapped_path(converted_path)
-    arm_result = arm_coordinates(converted_path)
-    print(converted_path)
+    robot_path_planner = RobotPathPlanner(image)
+
+    arm_result = robot_path_planner.run(path,image)
     
+    cv2.imshow("Image", contour_image)
+    cv2.imwrite('contour_image.jpg', contour_image)
+
+    while True:
+        key = cv2.waitKey(1)  # 1ms 대기
+        if key == ord('q'):  # 'q' 키의 ASCII 코드 확인
+            break
+
+    cv2.destroyAllWindows()
+
     return arm_result
-    
-    # cv2.imshow("Image", shrink_lip_image)
-    # # cv2.imwrite('contour_image.jpg', contour_image)
-
-    # while True:
-    #     key = cv2.waitKey(1)  # 1ms 대기
-    #     if key == ord('q'):  # 'q' 키의 ASCII 코드 확인
-    #         break
-
-    # cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--image", type=str, default="/home/addinedu/aris/123/aris-repo-5/module/image/25=11_Cartoonize Effect.jpg", help="입력 이미지 경로", required=False)
-    parser.add_argument("--mask_padding", type=float, default=23, help="", required=False)
-    parser.add_argument("--eye_scale_factor", type=float, default=1.3, help="눈 확대 비율", required=False)
-    parser.add_argument("--lip_scale_factor", type=float, default=0.7, help="입 축소 비율", required=False)
+    parser.add_argument("--image", type=str, default="/home/addinedu/aris/image copy 3.png", help="입력 이미지 경로", required=False)
+    parser.add_argument("--mask_padding", type=float, default=16, help="", required=False)
+    parser.add_argument("--eye_scale_factor", type=float, default=1.0, help="눈 확대 비율", required=False)
+    parser.add_argument("--lip_scale_factor", type=float, default=1.0, help="입 축소 비율", required=False)
     
     args= parser.parse_args()
     main(args)
--- module/arm_path.py
+++ module/arm_path.py
@@ -7,116 +7,93 @@ from scipy.special import comb
 from .path_planning import path_planning
 
 
+class RobotPathPlanner:
+    '''이미지 경로를 로봇 경로로 변환'''
+    def __init__(self,image):
 
+        self.converted_path = []
+        self.image = image
 
-# image_center = (a/2,b/2)
-# sticker_center = (alpha,beta)
-# sticker_origin = (m,n)
+    def convert_coordinates(self,path,image):
+        '''
+        이미지상의 path를 실제 로봇의 path로 변환 (x,y)
+        image_center = (a/2,b/2)
+        sticker_center = (alpha,beta)
+        sticker_origin = (m,n) (가장 왼쪽 위 좌표)
+        '''
+        alpha , beta ,n, m = 73.4 , 64.5 , -287,-68
 
-alpha , beta ,n, m = 73.4 , 64.5 , -287,-68
-
-def convert_coordinates(path,a,b):
-
-    '''
-    이미지상의 path를 실제 로봇의 path로 변환 (x,y)
-    '''
-
-    scale = (beta/b)
-    converted_path = []
-    converted_path.append([-218,10.2]) #시작점
-
-    for x,y in path[1:]:
-        if math.isnan(x):
-            converted_path.append(('up','up'))
+        if len(image.shape) == 3:
+            a,b,_ = image.shape
         else:
-            y,x = int(alpha+m+(x-a)*scale),int(beta+n+(y-b)*scale) #좌표 변환
-            if converted_path[-1] != (x,y): #중복 좌표 제거
-                if converted_path[-1] == ('up','up'):
-                    converted_path.append((x,y))
+            a,b = image.shape
+
+        scale = (beta/b)
+        self.converted_path.append([-218,10.2]) #시작점
+
+        for x,y in path[1:]:
+            if math.isnan(x):
+                self.converted_path.append(('up','up'))
+            else:
+                y,x = int(alpha+m+(x-a)*scale),int(beta+n+(y-b)*scale) #좌표 변환
+                if self.converted_path[-1] != (x,y): #중복 좌표 제거
+                    if self.converted_path[-1] == ('up','up'):
+                        self.converted_path.append((x,y))
+                    else:
+                        if (self.converted_path[-1][0] != x and abs(self.converted_path[-1][1] - y) > 5) or (self.converted_path[-1][1] != y and abs(self.converted_path[-1][0] - x) > 5): ## 2차이 이상
+                            self.converted_path.append((x,y))
+        
+    def del_up(self):
+        '''
+        불필요한 ('up','up') 제거
+        '''
+        arm_path = []
+        for i in range(len(self.converted_path)-1):
+            if self.converted_path[i][0] == 'up':
+                if abs(self.converted_path[i-1][0]-self.converted_path[i+1][0]) > 2 or abs(self.converted_path[i-1][1]-self.converted_path[i+1][1]) > 2 :
+                    arm_path.append(self.converted_path[i])
+            else :
+                arm_path.append(self.converted_path[i])
+        arm_path.append(('up','up')) 
+
+        self.converted_path = arm_path
+
+    def add_down(self):
+        i=1
+        while i <len(self.converted_path) -2 :
+            if self.converted_path[i][0] == 'up':
+                self.converted_path.insert(i+2,('down','down'))
+                i+=1
+            i+=1
+
+    def arm_coordinates(self):
+        
+        '''
+        (x,y)좌표를 [x,y,z,r,p,yaw]로 변환
+        '''
+
+        z,r,p,yaw = 164,130,85,-37.6 ##그림 그릴 때 초기 좌표 값
+
+        arm_coordinates = [[self.converted_path[0][0],self.converted_path[0][1],z,r,p,yaw]]
+
+        for (x,y) in self.converted_path[1:]:
+            if arm_coordinates[-1] == ['up']:
+                arm_coordinates.append([x,y,174,r,p,yaw])
+            else :    
+                if x == 'up':
+                    arm_coordinates.append(['up'])
+                elif x == 'down':
+                    arm_coordinates.append(['down'])
                 else:
-                    if (converted_path[-1][0] != x and abs(converted_path[-1][1] - y) > 1) or (converted_path[-1][1] != y and abs(converted_path[-1][0] - x) > 1): ## 2차이 이상
-                        converted_path.append((x,y))
-            
-            # if converted_path[-1] == ('up','up'):
-            #     converted_path.append((x,y))
-            #     converted_path.append(('down','down'))
-            # else:
-            #     if converted_path[-1] != (x,y): #중복 좌표 제거
-            #         converted_path.append((x,y))
-    #print(converted_path[0])
-    print(len(converted_path))
-    return converted_path
-
-# def del_coordinate(converted_path):
-#     del_converted_path = []
-#     for i in range(len(converted_path)):
-#         if i//2==0:
-#             x,y = 
-#             del_converted_path.append()
-
-
-
-
-def visualize_mapped_path(converted_path):
-        """
-        변환된 좌표를 종이에 시각화하는 함수.
-
-        :param converted_path: 변환된 좌표 리스트 [(x1, y1), (x2, y2), ...]
-        :param m: 종이의 왼쪽 위 x 좌표
-        :param n: 종이의 왼쪽 위 y 좌표
-        :param alpha: 종이 중심의 x 반지름
-        :param beta: 종이 중심의 y 반지름
-        """
-        paper_width = alpha
-        paper_height = beta
-
-        # 종이의 영역 설정
-        plt.figure(figsize=(8, 8))
-        plt.xlim(m, m + paper_width)
-        plt.ylim(n, n + paper_height)
-
-        # 종이의 경계 그리기
-        plt.gca().add_patch(plt.Rectangle((n, m), paper_width, paper_height, edgecolor='black', fill=False, linewidth=2))
-
-        # 변환된 경로 시각화
-        for x, y in converted_path:
-            if not x == str:
-                plt.scatter(x, y, c='red', s=10)  # 변환된 점을 빨간색으로 표시
-
-        # 종이 중심 표시
-        plt.scatter(m + alpha, n + beta, c='blue', label="Paper Center", s=50)
-
-        plt.title("Mapped Path on Paper")
-        plt.xlabel("X-axis")
-        plt.ylabel("Y-axis")
-        plt.legend()
-        plt.grid(True)
-        plt.show()
-
-
-def arm_coordinates(converted_path):
+                    arm_coordinates.append([x,y,z,r,p,yaw])
+        print('arm_coods =' , arm_coordinates)
     
-    '''
-    (x,y)좌표를 [x,y,z,r,p,yaw]로 변환
-    '''
-    z,r,p,yaw = 164,130,85,-37.6 ##그림 그릴 때 초기 좌표 값
-    arm_coordinates = []
-
-    for (x,y) in converted_path:
-        if x == 'up':
-            arm_coordinates.append(['up'])
-        elif x == 'down':
-            arm_coordinates.append(['down'])
-        else:
-            arm_coordinates.append([x,y,z,r,p,yaw])
-
-    # print(arm_coordinates)
-    return arm_coordinates
-    
-
-
-
-
-# -287,-68 164 130 85 -37.6
-# -158
-# -158 78.8
+    def run(self,path,image):
+        '''
+        전체 과정 실행
+        '''
+        self.convert_coordinates(path,image)
+        self.del_up()
+        self.add_down()
+        
+        return self.arm_coordinates()
